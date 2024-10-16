841f6f37f6559a96f1614d9a05d553c9d6a68a4b
Mitch <Mitch.Ngugi99@gmail.com>
2024-10-16 23:20:46 +0100

    dark mode changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -23,34 +23,21 @@ export const metadata = {
 
 function Hero() {
   return (
-    <div className="relative">
+    <div className="relative bg-gray-900">
       <Gradient className="absolute inset-2 bottom-0 rounded-4xl ring-1 ring-inset ring-black/5" />
       <Container className="relative">
-        <Navbar
-          // banner={
-          //   <Link
-          //     href="/blog/radiant-raises-100m-series-a-from-tailwind-ventures"
-          //     className="flex items-center gap-1 rounded-full bg-fuchsia-950/35 px-3 py-0.5 text-sm/6 font-medium text-white data-[hover]:bg-fuchsia-950/30"
-          //   >
-          //     Radiant raises $100M Series A from Tailwind Ventures
-          //     <ChevronRightIcon className="size-4" />
-          //   </Link>
-          // }
-        />
+        <Navbar />
         <div className="pb-24 pt-16 sm:pb-32 sm:pt-24 md:pb-48 md:pt-32">
-          <h1 className="font-display text-balance text-6xl/[0.9] font-medium tracking-tight text-gray-950 sm:text-8xl/[0.8] md:text-9xl/[0.8]">
+          <h1 className="font-display text-balance text-6xl/[0.9] font-medium tracking-tight text-white sm:text-8xl/[0.8] md:text-9xl/[0.8]">
             Pantheon
 Security.
           </h1>
-          <p className="mt-8 max-w-lg text-xl/7 font-medium text-gray-950/75 sm:text-2xl/8">
+          <p className="mt-8 max-w-lg text-xl/7 font-medium text-white/75 sm:text-2xl/8">
             Fortifying Legacy Systems with AI-Powered
 Cybersecurity.
           </p>
           <div className="mt-12 flex flex-col gap-x-6 gap-y-4 sm:flex-row">
-            <Button href="#">Get started</Button>
-            {/* <Button variant="secondary" href="/pricing">
-              See pricing
-            </Button> */}
+            <Button href="#" className="text-white">Get started</Button>
           </div>
         </div>
       </Container>
@@ -60,9 +47,9 @@ Cybersecurity.
 
 function FeatureSection() {
   return (
-    <div className="overflow-hidden">
+    <div className="overflow-hidden bg-gray-900">
       <Container className="pb-24">
-        <Heading as="h2" className="max-w-3xl">
+        <Heading as="h2" className="max-w-3xl text-white">
           A snapshot of your entire
 Threat Landscape.
         </Heading>
@@ -80,85 +67,85 @@ Threat Landscape.
   )
 }
 
-function BentoSection() {
-  return (
-    <Container>
-      <Subheading>HEIMDALL - MANAGE YOUR ATTACK SURFACE</Subheading>
-      <Heading as="h3" className="mt-2 max-w-3xl">
-        A comprehensive view of
-your entire attack surface.
-      </Heading>
-       <Subheading>Secure Every Legacy System. Heimdall protects your legacy systems with real-time AI-powered monitoring
-and threat detection.</Subheading>
-      
+// function BentoSection() {
+//   return (
+//     <Container className="bg-gray-900">
+//       <Subheading className="text-white">HEIMDALL - MANAGE YOUR ATTACK SURFACE</Subheading>
+//       <Heading as="h3" className="mt-2 max-w-3xl text-white">
+//         A comprehensive view of
+// your entire attack surface.
+//       </Heading>
+//       <Subheading className="text-white">Secure Every Legacy System. Heimdall protects your legacy systems with real-time AI-powered monitoring
+// and threat detection.</Subheading>
 
-      <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
-        <BentoCard
-          eyebrow="ATTACK SURFACE MANAGEMENT"
-          title="Gain Full Visibility"
-          description="Heimdall uses AI to map your infrastructure, discovering all assets across legacy and modern
-systems."
-          graphic={
-            <div className="h-80 bg-[url(/screenshots/profile.png)] bg-[size:1000px_560px] bg-[left_-109px_top_-112px] bg-no-repeat" />
-          }
-          fade={['bottom']}
-          className="max-lg:rounded-t-4xl lg:col-span-3 lg:rounded-tl-4xl"
-        />
-        <BentoCard
-          eyebrow="VULNERABILITY ASSESSMENT"
-          title="Stay One Step Ahead"
-          description="With AI-driven vulnerability detection, Heimdall ensures you’re always aware of potential risks
-lurking in your systems."
-          graphic={
-            <div className="absolute inset-0 bg-[url(/screenshots/competitors.png)] bg-[size:1100px_650px] bg-[left_-38px_top_-73px] bg-no-repeat" />
-          }
-          fade={['bottom']}
-          className="lg:col-span-3 lg:rounded-tr-4xl"
-        />
-        <BentoCard
-          eyebrow="MONITORING"
-          title="Built for 24/7 Protection"
-          description="Our platform continuously scans your network, identifying weaknesses in real-time and
-providing immediate insights via interactive dashboards."
-          graphic={
-            <div className="flex size-full pl-10 pt-10">
-              <Keyboard highlighted={['LeftCommand', 'LeftShift', 'D']} />
-            </div>
-          }
-          className="lg:col-span-2 lg:rounded-bl-4xl"
-        />
-        <BentoCard
-          eyebrow="SEAMLESS INTEGRATION"
-          title="Legacy Meets Modern Security"
-          description="Heimdall integrates modern security measures into your legacy infrastructure with zero
-disruption, ensuring continuous protection."
-          graphic={<LogoCluster />}
-          className="lg:col-span-2"
-        />
-        <BentoCard
-          eyebrow="CONSULTING"
-          title="Get Expert Support"
-          description="Pantheon Security offers expert consulting services to help you prioritize and remediate
-complex vulnerabilities in legacy systems."
-          graphic={<Map />}
-          className="max-lg:rounded-b-4xl lg:col-span-2 lg:rounded-br-4xl"
-        />
-      </div>
-    </Container>
-  )
-}
+//       <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
+//         <BentoCard
+//           eyebrow="ATTACK SURFACE MANAGEMENT"
+//           title="Gain Full Visibility"
+//           description="Heimdall uses AI to map your infrastructure, discovering all assets across legacy and modern
+// systems."
+//           graphic={
+//             <div className="h-80 bg-[url(/screenshots/profile.png)] bg-[size:1000px_560px] bg-[left_-109px_top_-112px] bg-no-repeat" />
+//           }
+//           fade={['bottom']}
+//           className="max-lg:rounded-t-4xl lg:col-span-3 lg:rounded-tl-4xl"
+//         />
+//         <BentoCard
+//           eyebrow="VULNERABILITY ASSESSMENT"
+//           title="Stay One Step Ahead"
+//           description="With AI-driven vulnerability detection, Heimdall ensures you’re always aware of potential risks
+// lurking in your systems."
+//           graphic={
+//             <div className="absolute inset-0 bg-[url(/screenshots/competitors.png)] bg-[size:1100px_650px] bg-[left_-38px_top_-73px] bg-no-repeat" />
+//           }
+//           fade={['bottom']}
+//           className="lg:col-span-3 lg:rounded-tr-4xl"
+//         />
+//         <BentoCard
+//           eyebrow="MONITORING"
+//           title="Built for 24/7 Protection"
+//           description="Our platform continuously scans your network, identifying weaknesses in real-time and
+// providing immediate insights via interactive dashboards."
+//           graphic={
+//             <div className="flex size-full pl-10 pt-10">
+//               {/* <Keyboard highlighted={['LeftCommand', 'LeftShift', 'D']} /> */}
+//               <LogoTimeline />
+//             </div>
+//           }
+//           className="lg:col-span-2 lg:rounded-bl-4xl"
+//         />
+//         <BentoCard
+//           eyebrow="SEAMLESS INTEGRATION"
+//           title="Legacy Meets Modern Security"
+//           description="Heimdall integrates modern security measures into your legacy infrastructure with zero
+// disruption, ensuring continuous protection."
+//           graphic={<LogoCluster />}
+//           className="lg:col-span-2"
+//         />
+//         <BentoCard
+//           eyebrow="CONSULTING"
+//           title="Get Expert Support"
+//           description="Pantheon Security offers expert consulting services to help you prioritize and remediate
+// complex vulnerabilities in legacy systems."
+//           graphic={<Map />}
+//           className="max-lg:rounded-b-4xl lg:col-span-2 lg:rounded-br-4xl"
+//         />
+//       </div>
+//     </Container>
+//   )
+// }
 
 function DarkBentoSection() {
   return (
-    <div className="mx-2 mt-2 rounded-4xl bg-gray-900 py-32">
+    <div className="mx-2 mt-2 rounded-4xl bg-black py-32">
       <Container>
-        <Subheading dark>METIS – STRATEGIC CYBERSECURITY CONSULTANCY</Subheading>
-        <Heading as="h3" dark className="mt-2 max-w-2xl">
+        <Subheading dark className="text-white">METIS – STRATEGIC CYBERSECURITY CONSULTANCY</Subheading>
+        <Heading as="h3" dark className="mt-2 max-w-2xl text-white">
           Tailored strategies for
 comprehensive legacy
 system protection.
         </Heading>
-        <Subheading dark>Transform Your Security Approach. Metis provides expert consulting to help you secure even the most
+        <Subheading dark className="text-white">Transform Your Security Approach. Metis provides expert consulting to help you secure even the most
 complex infrastructures.</Subheading>
 
         <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
@@ -213,15 +200,15 @@ business grows."
 
 export default function Home() {
   return (
-    <div className="overflow-hidden">
+    <div className="overflow-hidden bg-black">
       <Hero />
       <main>
         <Container className="mt-10">
           {/* <LogoCloud /> */}
         </Container>
-        <div className="bg-gradient-to-b from-white from-50% to-gray-100 py-32">
+        <div className="bg-gradient-to-b from-gray-900 from-50% to-black py-32">
           <FeatureSection />
-          <BentoSection />
+          {/* <BentoSection /> */}
         </div>
         <DarkBentoSection />
       </main>
