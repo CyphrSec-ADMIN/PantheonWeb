04753f8ae1b9bbd84a355f898ce60a2ef798532e
Mitch <Mitch.Ngugi99@gmail.com>
2024-10-23 06:47:17 +0100

    map assets

--- src/components/logo-cluster.jsx
+++ src/components/logo-cluster.jsx
@@ -38,7 +38,6 @@ function Circles() {
       <Circle size={400} opacity="5%" delay={0.3} />
       <Circle size={272} opacity="5%" delay={0.15} />
       <Circle size={144} opacity="10%" delay={0} />
-      <div className="absolute inset-0 bg-gradient-to-t from-white to-35%" />
     </div>
   )
 }
